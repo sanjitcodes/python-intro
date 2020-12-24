3fe27b48e9e270231a7c401c2ac4e2389857e12a
SANJIT ANAND <59444668+sanjitcodes@users.noreply.github.com>
2020-12-24 19:48:53 +0530

    initial commit

---
+++ conditions.py
@@ -0,0 +1,8 @@
+a = 15
+if a > 15:
+    print(f"Greater than {a}")
+elif a < 15:
+    print(f"Smaller than {a}")
+else:
+    print(f"Equal to {a}")
+    
---
+++ list.py
@@ -0,0 +1,4 @@
+food = ["Rajma Chawal", "Chole Bhature", "Gulab Jamun", "Lassi", "Kachori", "15"]
+
+for name in food:
+    print(name)
